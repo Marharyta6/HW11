4c51759c9cb907d603accfb1ac01911b66670bd7
Marharyta6 <131263078+Marharyta6@users.noreply.github.com>
2023-07-21 15:50:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,111 +1,5 @@
-from collections import UserDict
-from datetime import datetime, timedelta
+from classes import AddressBook, Name, Phone, Record, Birthday
 
-class Field:
-    def __init__(self, value):
-        self._value = None
-        self.value = value
-
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, new_value):
-        self._value = new_value
-
-    def __str__(self) -> str:
-        return self.value
-
-    def __repr__(self) -> str:
-        return str(self)
-
-class Name(Field):
-    pass
-
-
-class Phone(Field):
-    @Field.value.setter
-    def value(self, new_value):
-        # if len(new_value) != 13 or not new_value.startswith("+38"):
-        #     raise ValueError("Invalid phone number")
-        self._value = new_value
-
-
-class Birthday(Field):
-    @Field.value.setter
-    def value(self, new_value):
-        try:
-            datetime.strptime(new_value, "%d-%m-%Y")
-        except ValueError:
-            raise ValueError("Invalid birthday format")
-        self._value = new_value
-
-    @property
-    def date(self):
-        return datetime.strptime(self.value, "%d-%m-%Y").date()
-
-
-class Record:
-    def __init__(self, name: Name, 
-                 phone: Phone = None, 
-                 birthday: Birthday = None) -> None:
-        self.name = name
-        self.phones = []
-        self.birthday = birthday
-        if phone:
-            self.phones.append(phone)
-
-    def add_phone(self, phone: Phone):
-        if phone.value not in [p.value for p in self.phones]:
-            self.phones.append(phone)
-            return f"phone {phone} add to contact {self.name}"
-        return f"{phone} present in phones of contact {self.name}"
-
-    def change_phone(self, old_phone, new_phone):
-        for idx, p in enumerate(self.phones):
-            if old_phone.value == p.value:
-                self.phones[idx] = new_phone
-                return f"old phone {old_phone} change to {new_phone}"
-        return f"{old_phone} not present in phones of contact {self.name}"
-    
-    def days_to_birthday(self):
-        if self.birthday:
-            today = datetime.now().date()
-            next_birthday = datetime(
-                today.year, self.birthday.date.month, self.birthday.date.day).date()
-            if next_birthday < today:
-                next_birthday = datetime(
-                    today.year + 1, self.birthday.date.month, self.birthday.date.day).date()
-            days_left = (next_birthday - today).days
-            return f"Days until the next birthday of {self.name}: {days_left}"
-        else:
-            return f"No birthday set for contact {self.name}"
-
-    def __str__(self) -> str:
-        return f"{self.name}: {', '.join(str(p) for p in self.phones)}, {self.birthday}"
-    
-
-class AddressBook(UserDict):
-    def add_record(self, record: Record):
-        self.data[str(record.name)] = record
-        return f"Contact {record} add success"
-    
-    def iterator(self, rec_per_page=3):
-        record_counter = 0
-        result = ""
-        for record in self.values():
-            result += str(record) + '\n'
-            record_counter += 1
-            if record_counter >= rec_per_page:
-                yield result 
-                record_counter = 0
-                result = ""
-        if result:
-            yield result
-
-    def __str__(self) -> str:
-        return "\n".join(str(r) for r in self.data.values())
 
 
 def input_error(func):
@@ -125,7 +19,7 @@ address_book = AddressBook()
 def add_contact(*args):
     name = Name(args[0])
     phone = Phone(args[1]) if len(args) > 2 else None
-    birthday = Birthday(args[2]) if len(args) > 3 else None
+    birthday = Birthday(args[2]) #if len(args) > 3 else None
     rec: Record = address_book.get(str(name))
     if rec:
         return rec.add_phone(phone)
@@ -165,6 +59,15 @@ def show_all_contacts(*args):
         return result
     return address_book
 
+@input_error
+def get_birthday(*args):
+    name = Name(args[0])
+    rec: Record = address_book.get(str(name))
+    if rec:
+        return rec.days_to_birthday(name)
+    else:
+        raise KeyError(f"Contact '{name}' not found.")
+
 
 def greeting_command(*args):
     return "How can I help you?"
@@ -182,6 +85,7 @@ COMMANDS = {add_contact: ("add", ),
             change_phone: ("change",),
             get_phone: ("phone",),
             show_all_contacts: ("show all", ),
+            get_birthday: ("birthday", ),
             greeting_command: ("hello", ),
             exit_command: ("good bye", "close", "exit"),
             }
@@ -209,7 +113,7 @@ def main():
 
 
 if __name__ == "__main__":
-    for _ in range(10):
-        rec1 = Record(Name(f"Bill_{_}"),Phone("+380997663345"),Birthday("20-10-2015"))
-        address_book.add_record(rec1)
+    # for _ in range(10):
+    #     rec1 = Record(Name(f"Bill_{_}"),Phone("+380997663345"),Birthday("20-10-2015"))
+    #     address_book.add_record(rec1)
     main()
